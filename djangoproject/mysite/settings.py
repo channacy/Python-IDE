ab56e3a8bdc9cc874426ad11b2c5d6edf47827f8
Channacy <68759286+channacy@users.noreply.github.com>
2025-01-05 15:50:14 -0500

    added styling

--- djangoproject/mysite/settings.py
+++ djangoproject/mysite/settings.py
@@ -135,3 +135,10 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# For production
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'your-email@gmail.com'
+# EMAIL_HOST_PASSWORD = 'your-email-password'
